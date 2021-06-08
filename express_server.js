7622a15043bc3b2954af438391aec1c02ac6e102
Alex Li <lialex0218@gmail.com>
2021-06-08 00:10:26 +0000

    express server with render

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,8 +15,10 @@ app.get("/", (req, res) => {
 
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase};
+  //res.json(urlDatabase);
+  res.render("urls_index", templateVars);
   
 });
 
